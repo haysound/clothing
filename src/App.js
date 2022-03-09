7fb28d0f42831f2cdb8facbb341633b4f2d0878b
Ayomide Ayansiji <ayomideayansiji@ymail.com>
2022-03-09 15:36:10 +0100

    updated with shop page and collections components

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import React from "react";
 import "./App.css";
 import { Route, Switch } from "react-router";
 import Homepage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <Switch>
       <Route exact="true" path="/" component={Homepage} />
-      <Route path="/hats" component={HatsPage} />
+      <Route path="/shop" component={ShopPage} />
     </Switch>
   );
 }
