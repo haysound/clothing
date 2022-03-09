15f8d9a62baf1135fe8b33a8e87f8c2216d00da2
Ayomide Ayansiji <ayomideayansiji@ymail.com>
2022-03-09 17:05:46 +0100

    added Header component to pages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 import { Route, Switch } from "react-router";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
+import Header from "./components/header/header.component";
 function App() {
   return (
-    <Switch>
-      <Route exact="true" path="/" component={Homepage} />
-      <Route path="/shop" component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact="true" path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
