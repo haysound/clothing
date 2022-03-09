c07beb6bc9659f4c8acd3276607d521594f96ad1
Ayomide Ayansiji <ayomideayansiji@ymail.com>
2022-03-09 12:58:15 +0100

    created react route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./App.css";
-
+import { Route, Switch } from "react-router";
 import Homepage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Switch>
+      <Route exact="true" path="/" component={Homepage} />
+      <Route path="/hats" component={HatsPage} />
+    </Switch>
   );
 }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { withRouter } from "react-router";
 import './menu-item.style.scss';
 
-const MenuItem = ({title, imageUrl, size}) =>{
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) =>{
     return (
-        <div className={`${size} menu-item`}>
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div className="background-image" style={{backgroundImage:`url(${imageUrl})`}}/>
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
@@ -13,4 +13,4 @@ const MenuItem = ({title, imageUrl, size}) =>{
         </div>
     )
 }
-export default MenuItem;
+export default withRouter(MenuItem);
